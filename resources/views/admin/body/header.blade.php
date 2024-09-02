7fde3bb42d87cc5d5f475effcddf18894a4b864d
Kezia Megawati <keziamega13@gmail.com>
2024-09-02 15:45:51 +0700

    buat tombol keluar

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -3,19 +3,11 @@
     <i class="fe fe-menu navbar-toggler-icon"></i>
   </button>
 
-  <ul class="nav">
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle text-muted pr-0" href="#" id="navbarDropdownMenuLink" role="button"
-        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <span class="avatar avatar-sm mt-2">
-          <img src="{{asset('backend/assets/avatars/face-1.jpg')}}" alt="..." class="avatar-img rounded-circle">
-        </span>
+  <ul class="nav ml-auto">
+    <li class="nav-item">
+      <a class="nav-link text-muted pr-0" href="{{route('admin.logout')}}">
+        <i class="fe fe-log-out"></i> Keluar
       </a>
-      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-        <a class="dropdown-item" href="#">Profile</a>
-        <a class="dropdown-item" href="#">Settings</a>
-        <a class="dropdown-item" href="{{route('admin.logout')}}">Keluar</a>
-      </div>
     </li>
   </ul>
 </nav>

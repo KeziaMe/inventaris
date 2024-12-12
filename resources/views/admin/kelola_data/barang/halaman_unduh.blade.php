0180b7846417369b203facc227045aa7b01868f1
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-12-12 15:33:56 +0700

    buat tampilan halaman unduh sementara

--- resources/views/admin/kelola_data/barang/halaman_unduh.blade.php
+++ resources/views/admin/kelola_data/barang/halaman_unduh.blade.php
@@ -1,6 +1,33 @@
 @extends('admin.admin_master')
 @section('admin')
+<main role="main" class="main-content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Unduh Barang</h3>
+                    </div>
+                    <div class="card-body">
+                        <form action="#" method="GET">
+                            @csrf
+                            <div class="form-group">
+                                <label for="ruangan">Pilih Ruangan</label>
+                                <select name="ruangan" id="ruangan" class="form-control" required>
+                                    <option value="">-- Pilih Ruangan --</option>
 
-<h1>HALAMAN UNDUH</h1>
+                                    <option value=""></option>
 
+                                </select>
+                            </div>
+                            <div class="form-group mt-3">
+                                <button type="submit" class="btn btn-primary">Unduh Barang</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 @endsection

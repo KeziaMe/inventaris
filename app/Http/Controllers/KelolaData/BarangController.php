5ed42c223a6ef8e475ff02c1608dad3c175f2c49
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2025-01-13 15:57:58 +0700

    buat fungsi unduh sementara

--- app/Http/Controllers/KelolaData/BarangController.php
+++ app/Http/Controllers/KelolaData/BarangController.php
@@ -161,7 +161,8 @@ class BarangController extends Controller
         $allDataBarang = $query->get();
 
         // Generate PDF
-        $pdf = PDF::loadView('admin.barang.download', compact('allDataBarang', 'ruangan'));
+        $pdf = PDF::loadView('admin.kelola_data.barang.unduhPDF', compact('allDataBarang', 'ruangan'));
+
 
         // Return PDF as download
         return $pdf->download('barang_' . $ruangan . '_unduh.pdf');
